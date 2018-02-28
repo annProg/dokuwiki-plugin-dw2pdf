8484772b8ce6682ddf1d79fdfe5640471acfb317
annprog <tecbbs@qq.com>
2018-02-28 15:54:09 +0800

    update mpdf

--- vendor/mpdf/mpdf/src/Image/ImageProcessor.php
+++ vendor/mpdf/mpdf/src/Image/ImageProcessor.php
@@ -4,6 +4,7 @@ namespace Mpdf\Image;
 
 use Mpdf\Cache;
 use Mpdf\Color\ColorConverter;
+use Mpdf\Color\ColorModeConverter;
 use Mpdf\CssManager;
 use Mpdf\Gif\Gif;
 use Mpdf\Language\LanguageToFontInterface;
@@ -15,7 +16,7 @@ use Mpdf\SizeConverter;
 
 use Psr\Log\LoggerInterface;
 
-class ImageProcessor
+class ImageProcessor implements \Psr\Log\LoggerAwareInterface
 {
 
 	/**
@@ -42,6 +43,11 @@ class ImageProcessor
 	 * @var \Mpdf\Color\ColorConverter
 	 */
 	private $colorConverter;
+
+	/**
+	 * @var \Mpdf\Color\ColorModeConverter
+	 */
+	private $colorModeConverter;
 
 	/**
 	 * @var \Mpdf\Cache
@@ -89,6 +95,7 @@ class ImageProcessor
 		CssManager $cssManager,
 		SizeConverter $sizeConverter,
 		ColorConverter $colorConverter,
+		ColorModeConverter $colorModeConverter,
 		Cache $cache,
 		LanguageToFontInterface $languageToFont,
 		ScriptToLanguageInterface $scriptToLanguage,
@@ -100,14 +107,28 @@ class ImageProcessor
 		$this->cssManager = $cssManager;
 		$this->sizeConverter = $sizeConverter;
 		$this->colorConverter = $colorConverter;
+		$this->colorModeConverter = $colorModeConverter;
 		$this->cache = $cache;
 		$this->languageToFont = $languageToFont;
 		$this->scriptToLanguage = $scriptToLanguage;
+
 		$this->logger = $logger;
 
 		$this->guesser = new ImageTypeGuesser();
 
 		$this->failedImages = [];
+	}
+
+	/**
+	 * @param \Psr\Log\LoggerInterface
+	 *
+	 * @return \Mpdf\Mpdf
+	 */
+	public function setLogger(LoggerInterface $logger)
+	{
+		$this->logger = $logger;
+
+		return $this;
 	}
 
 	public function getImage(&$file, $firsttime = true, $allowvector = true, $orig_srcpath = false, $interpolation = false)
@@ -139,7 +160,7 @@ class ImageProcessor
 				$orig_srcpath = str_replace(" ", "%20", $orig_srcpath);
 			}
 			if (!preg_match('/^(http|ftp)/', $orig_srcpath)) {
-				$orig_srcpath = urldecode_parts($orig_srcpath);
+				$orig_srcpath = $this->urldecodeParts($orig_srcpath);
 			}
 		}
 
@@ -209,7 +230,7 @@ class ImageProcessor
 
 		// SVG
 		if ($type == 'svg') {
-			$svg = new Svg($this->mpdf, $this->otl, $this->cssManager, $this->sizeConverter, $this->colorConverter, $this->languageToFont, $this->scriptToLanguage);
+			$svg = new Svg($this->mpdf, $this->otl, $this->cssManager, $this, $this->sizeConverter, $this->colorConverter, $this->languageToFont, $this->scriptToLanguage);
 			$family = $this->mpdf->FontFamily;
 			$style = $this->mpdf->FontStyle;
 			$size = $this->mpdf->FontSizePt;
@@ -1108,7 +1129,7 @@ class ImageProcessor
 						$trns[2] = $this->translateValue(substr($t, 4, 2), $bpc);
 						$trnsrgb = $trns;
 						if ($targetcs == 'DeviceCMYK') {
-							$col = $this->colorConverter->rgb2cmyk([3, $trns[0], $trns[1], $trns[2]]);
+							$col = $this->colorModeConverter->rgb2cmyk([3, $trns[0], $trns[1], $trns[2]]);
 							$c1 = intval($col[1] * 2.55);
 							$c2 = intval($col[2] * 2.55);
 							$c3 = intval($col[3] * 2.55);
@@ -1128,7 +1149,7 @@ class ImageProcessor
 							$trns = [$r, $g, $b]; // ****
 							$trnsrgb = $trns;
 							if ($targetcs == 'DeviceCMYK') {
-								$col = $this->colorConverter->rgb2cmyk([3, $r, $g, $b]);
+								$col = $this->colorModeConverter->rgb2cmyk([3, $r, $g, $b]);
 								$c1 = intval($col[1] * 2.55);
 								$c2 = intval($col[2] * 2.55);
 								$c3 = intval($col[3] * 2.55);
@@ -1157,7 +1178,7 @@ class ImageProcessor
 					}
 
 					if ($targetcs == 'DeviceCMYK') {
-						$col = $this->colorConverter->rgb2cmyk([3, $r, $g, $b]);
+						$col = $this->colorModeConverter->rgb2cmyk([3, $r, $g, $b]);
 						$c1 = intval($col[1] * 2.55);
 						$c2 = intval($col[2] * 2.55);
 						$c3 = intval($col[3] * 2.55);
@@ -1327,5 +1348,26 @@ class ImageProcessor
 
 		$this->logger->warning(sprintf('%s (%s)', $msg, $file), ['context' => LogContext::IMAGES]);
 	}
+
+	/**
+	 * @since mPDF 5.7.4
+	 * @param string $url
+	 * @return string
+	 */
+	private function urldecodeParts($url)
+	{
+		$file = $url;
+		$query = '';
+		if (preg_match('/[?]/', $url)) {
+			$bits = preg_split('/[?]/', $url, 2);
+			$file = $bits[0];
+			$query = '?' . $bits[1];
+		}
+		$file = rawurldecode($file);
+		$query = urldecode($query);
+
+		return $file . $query;
+	}
+
 
 }
